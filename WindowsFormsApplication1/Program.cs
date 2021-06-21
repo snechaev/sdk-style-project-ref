00a5f1aa15074d1d93a0fc035cfc14c9e282a31a
Sergey Nechaev <nechaev@vicman.net>
2021-06-21 20:12:00 +0700

    init

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PhotoTestTool2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
